3403b09e6bbb54721b527f4d2d7e31785eebc94a
Alexey Terentyev <axifnx@gmail.com>
2020-02-25 13:12:21 +0300

    Fix Exception type
    Signed-off-by: Alexey Terentyev <axifnx@gmail.com>

--- src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
@@ -3,7 +3,7 @@ package ru.tinkoff.fintech.service.notification
 class CardNumberMaskerImpl: CardNumberMasker {
 
     override fun mask(cardNumber: String, maskChar: Char, start: Int, end: Int): String {
-        if (start > end) throw Exception("Start index cannot be greater than end index")
+        if (start > end) throw IndexOutOfBoundsException("Start index cannot be greater than end index")
         if (cardNumber.isBlank() || start == end) return cardNumber
         val endIndex = if (end > cardNumber.length) cardNumber.length else end
         return cardNumber.replaceRange(start, endIndex, maskChar.toString().repeat(endIndex - start))
