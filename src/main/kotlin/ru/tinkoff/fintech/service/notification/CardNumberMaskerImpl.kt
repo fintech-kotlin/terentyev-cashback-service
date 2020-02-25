e887ce58e11ec3d13eb74ef7ab800531d4f8ccae
Alexey Terentyev <axifnx@gmail.com>
2020-02-25 16:41:16 +0300

    Change as recommended
    Signed-off-by: Alexey Terentyev <axifnx@gmail.com>

--- src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
@@ -2,10 +2,13 @@ package ru.tinkoff.fintech.service.notification
 
 class CardNumberMaskerImpl: CardNumberMasker {
 
-    override fun mask(cardNumber: String, maskChar: Char, start: Int, end: Int): String {
-        if (start > end) throw IndexOutOfBoundsException("Start index cannot be greater than end index")
-        if (cardNumber.isBlank() || start == end) return cardNumber
-        val endIndex = if (end > cardNumber.length) cardNumber.length else end
-        return cardNumber.replaceRange(start, endIndex, maskChar.toString().repeat(endIndex - start))
-    }
+    override fun mask(cardNumber: String, maskChar: Char, start: Int, end: Int): String =
+        when {
+            start > end -> throw IndexOutOfBoundsException("Start index cannot be greater than end index")
+            cardNumber.isBlank() || start == end -> cardNumber
+            else -> {
+                val endIndex = if (end > cardNumber.length) cardNumber.length else end
+                cardNumber.replaceRange(start, endIndex, maskChar.toString().repeat(endIndex - start))
+            }
+        }
 }
