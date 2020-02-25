8de056440c88d7a8e73321e757fab24dae21b643
Alexey Terentyev <axifnx@gmail.com>
2020-02-25 12:57:55 +0300

    Implement generateMessage and mask methods
    Signed-off-by: Alexey Terentyev <axifnx@gmail.com>

--- src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
@@ -3,6 +3,9 @@ package ru.tinkoff.fintech.service.notification
 class CardNumberMaskerImpl: CardNumberMasker {
 
     override fun mask(cardNumber: String, maskChar: Char, start: Int, end: Int): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (start > end) throw Exception("Start index cannot be greater than end index")
+        if (cardNumber.isBlank() || start == end) return cardNumber
+        val endIndex = if (end > cardNumber.length) cardNumber.length else end
+        return cardNumber.replaceRange(start, endIndex, maskChar.toString().repeat(endIndex - start))
     }
 }
