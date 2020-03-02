85e7691661382c7892b600458ea633ed496651e3
Alexey Terentyev <axifnx@gmail.com>
2020-03-02 17:04:00 +0300

    Refactoring. Add constants
    Signed-off-by: Alexey Terentyev <axifnx@gmail.com>

--- src/main/kotlin/ru/tinkoff/fintech/service/cashback/CashbackCalculatorImpl.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/cashback/CashbackCalculatorImpl.kt
@@ -17,80 +17,88 @@ internal const val MAX_CASH_BACK = 3000.0
 internal const val MCC_SOFTWARE = 5734
 internal const val MCC_BEER = 5921
 
+
+private const val PERCENT_BLACK_DEFAULT = 1
+
+private const val PERCENT_BEER_FIRST_LAST_NAME = 10
+private const val PERCENT_BEER_FIRSTNAME = 7
+private const val PERCENT_BEER_MONTH_LETTER = 5
+private const val PERCENT_BEER_PREV_NEXT_MONTH_LETTER = 3
+private const val PERCENT_BEER_DEFAULT = 2
+
+private const val BEER_FIRSTNAME = "олег"
+private const val BEER_LASTNAME  = "олегов"
+
+private const val EXTRA_BONUS_TRIPLE_SIX = 666.0
+
+
 class CashbackCalculatorImpl : CashbackCalculator {
 
     private val addBonus: BigDecimal.(transactionSum: BigDecimal, percent: BigDecimal) -> BigDecimal =
         { transactionSum, percent -> this + (transactionSum * percent).divide(BigDecimal(100)) }
 
-    override fun calculateCashback(transactionInfo: TransactionInfo): Double {
-        with(transactionInfo) {
-            var cashback = when (loyaltyProgramName) {
-                LOYALTY_PROGRAM_BLACK -> loyaltyBlack(this)
-                LOYALTY_PROGRAM_ALL   -> loyaltyAll(this)
-                LOYALTY_PROGRAM_BEER  -> loyaltyBeer(this)
-                else                  -> BigDecimal.ZERO
-            }
-            cashback += extraBonus(this)
-
-            // checks if the total cashback has exceeded 3000
-            if ((BigDecimal(cashbackTotalValue) + cashback) > BigDecimal(3000)) {
-                cashback = BigDecimal(3000) - BigDecimal(cashbackTotalValue)
-            }
-
-            return cashback.setScale(2, RoundingMode.CEILING).toDouble()
+    override fun calculateCashback(transactionInfo: TransactionInfo): Double = with(transactionInfo) {
+        var cashback = when (loyaltyProgramName) {
+            LOYALTY_PROGRAM_BLACK -> loyaltyBlack(this)
+            LOYALTY_PROGRAM_ALL   -> loyaltyAll(this)
+            LOYALTY_PROGRAM_BEER  -> loyaltyBeer(this)
+            else                  -> BigDecimal.ZERO
         }
+        cashback += extraBonus(this)
+
+        // checks if the total cashback has exceeded 3000
+        if ((BigDecimal(cashbackTotalValue) + cashback) > BigDecimal(3000)) {
+            cashback = BigDecimal(3000) - BigDecimal(cashbackTotalValue)
+        }
+
+        return cashback.setScale(2, RoundingMode.CEILING).toDouble()
     }
 
     private fun loyaltyBlack(transactionInfo: TransactionInfo): BigDecimal {
         val transactSum = BigDecimal.valueOf(transactionInfo.transactionSum)
         val cashback = BigDecimal.ZERO
 
-        return cashback.addBonus(transactSum, BigDecimal.ONE)
+        return cashback.addBonus(transactSum, BigDecimal(PERCENT_BLACK_DEFAULT))
     }
 
-    private fun loyaltyAll(transactionInfo: TransactionInfo): BigDecimal {
-        with(transactionInfo) {
-            val transactSum = BigDecimal.valueOf(transactionSum)
-            var cashback = BigDecimal.ZERO
+    private fun loyaltyAll(transactionInfo: TransactionInfo): BigDecimal = with(transactionInfo) {
+        val transactSum = BigDecimal.valueOf(transactionSum)
+        var cashback = BigDecimal.ZERO
 
-            if (mccCode == MCC_SOFTWARE && isPalindrome(transactionSum)) {
-                val lcm = lcm(firstName.length, lastName.length)
-                cashback = cashback.addBonus(transactSum, BigDecimal(lcm).divide(BigDecimal(1000)))
-            }
-
-            return cashback
+        if (mccCode == MCC_SOFTWARE && isPalindrome(transactionSum)) {
+            val lcm = lcm(firstName.length, lastName.length)
+            cashback = cashback.addBonus(transactSum, BigDecimal(lcm).divide(BigDecimal(1000)))
         }
+
+        return cashback
     }
 
-    private fun loyaltyBeer(transactionInfo: TransactionInfo): BigDecimal {
-        with(transactionInfo) {
-            val transactSum = BigDecimal.valueOf(transactionInfo.transactionSum)
-            var cashback = BigDecimal.ZERO
+    private fun loyaltyBeer(transactionInfo: TransactionInfo): BigDecimal = with(transactionInfo) {
+        val transactSum = BigDecimal.valueOf(transactionInfo.transactionSum)
+        val cashback = BigDecimal.ZERO
 
-            if (mccCode == MCC_BEER) {
-                val lowerName = firstName.toLowerCase()
-
-                val percent = when {
-                    lowerName == "олег" && lastName.toLowerCase() == "олегов"         -> BigDecimal(10)
-                    lowerName == "олег"                                               -> BigDecimal(7)
-                    lowerName.startsWith(getMonthLetter())                            -> BigDecimal(5)
-                    lowerName.startsWith(getMonthLetter(1L))
-                            || lowerName.startsWith(getMonthLetter(-1L))              -> BigDecimal(3)
-                    else                                                              -> BigDecimal(2)
-                }
-                cashback = cashback.addBonus(transactSum, percent)
-            }
-
+        if (mccCode != MCC_BEER) {
             return cashback
         }
 
+        val lowerName = firstName.toLowerCase()
+
+        val percent = when {
+            lowerName == BEER_FIRSTNAME && lastName.toLowerCase() == BEER_LASTNAME -> BigDecimal(PERCENT_BEER_FIRST_LAST_NAME)
+            lowerName == BEER_FIRSTNAME                                            -> BigDecimal(PERCENT_BEER_FIRSTNAME)
+            lowerName.startsWith(getMonthLetter())                                 -> BigDecimal(PERCENT_BEER_MONTH_LETTER)
+            lowerName.startsWith(getMonthLetter(1L))
+                    || lowerName.startsWith(getMonthLetter(-1L))                   -> BigDecimal(PERCENT_BEER_PREV_NEXT_MONTH_LETTER)
+            else                                                                   -> BigDecimal(PERCENT_BEER_DEFAULT)
+        }
+        return cashback.addBonus(transactSum, percent)
     }
 
     private fun extraBonus (transactionInfo: TransactionInfo): BigDecimal {
         var cashback = BigDecimal.ZERO
         with(transactionInfo) {
-            if (transactionSum == 666.0 || transactionSum % 666 == 0.0) {
-                cashback += BigDecimal(666).divide(BigDecimal(100))
+            if (transactionSum == EXTRA_BONUS_TRIPLE_SIX || transactionSum % EXTRA_BONUS_TRIPLE_SIX == 0.0) {
+                cashback += BigDecimal(EXTRA_BONUS_TRIPLE_SIX).divide(BigDecimal(100))
             }
         }
         return cashback
